6b66649c4b69e46539b5aba715780fba562347cc
Robert Adamczewski <robert@adamczewski.me>
2020-02-25 20:45:16 +0100

    feat: create tests for checking input single characters

--- src/__tests__/caesar.test.js
+++ src/__tests__/caesar.test.js
@@ -17,5 +17,29 @@ describe('Caesar cipher ROT13 function', () => {
 		});
 	});
 
-	describe('when ciphering input', () => {});
+	describe('when ciphering input single character', () => {
+		const upperCaseChar = 'L';
+		const stringNumber = '1';
+		const numberNumber = 1;
+		const toBeMatchString = 'a';
+		const someCharToBeFoundInArray = 'j';
+		const indexOfFn = jest.fn((char, arr) => arr.indexOf(char.toLowerCase()));
+		// prettier-ignore
+		const alphabet = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
+		test('is upper case', () => {
+			expect(upperCaseChar === upperCaseChar.toUpperCase()).toBeTruthy();
+		});
+		test('is not a number', () => {
+			expect(typeof stringNumber === 'number').toBeFalsy();
+		});
+		test('is a number', () => {
+			expect(typeof numberNumber === 'number').toBeTruthy();
+		});
+		test('matches pattern /[a-z]|[A-Z]/g', () => {
+			expect(toBeMatchString).toMatch(/[a-z]|[A-Z]/g);
+		});
+		test('find indexOf lowercase character inside the alphabet array', () => {
+			expect(indexOfFn(someCharToBeFoundInArray, alphabet)).not.toBe(-1);
+		});
+	});
 });
