40a537bca8a18774f6d506c742004b0e0b70872b
Robert Adamczewski <robert@adamczewski.me>
2020-02-26 18:28:33 +0100

    refactor: change tests to describe as the function should work

--- src/__tests__/caesar.test.js
+++ src/__tests__/caesar.test.js
@@ -1,50 +1,84 @@
 const caesar13 = require('../js/caesar13');
+const rot = 13;
 
 describe('Caesar cipher ROT13 function', () => {
 	describe('when not string', () => {
 		test('input is not typeof string', () => {
+			const input = 12345;
 			expect(() => {
-				caesar13(12233);
+				caesar13(input, rot);
 			}).toThrow(TypeError);
 		});
 	});
 
 	describe('when string', () => {
 		test('input is empty', () => {
+			const input = '';
 			expect(() => {
-				caesar13('');
+				caesar13(input, rot);
 			}).toThrow(Error);
 		});
 	});
 
 	describe('when ciphering input single character', () => {
-		const upperCaseChar = 'L';
-		const stringNumber = '1';
-		const numberNumber = 1;
-		const toBeMatchString = 'a';
-		const someCharToBeFoundInArray = 'j';
+		const inputWord = 'a1b';
+		const inputWordArray = [
+			...inputWord
+		];
 		const indexOfFn = jest.fn((char, arr) => arr.indexOf(char.toLowerCase()));
 		// prettier-ignore
 		const alphabet = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
-		test('is upper case', () => {
-			expect(upperCaseChar === upperCaseChar.toUpperCase()).toBeTruthy();
-		});
-		test('is not a number', () => {
-			expect(typeof stringNumber === 'number').toBeFalsy();
-		});
-		test('is a number', () => {
-			expect(typeof numberNumber === 'number').toBeTruthy();
-		});
-		test('matches pattern /[a-z]|[A-Z]/g', () => {
-			expect(toBeMatchString).toMatch(/[a-z]|[A-Z]/g);
-		});
-		test('find indexOf lowercase character inside the alphabet array', () => {
-			expect(indexOfFn(someCharToBeFoundInArray, alphabet)).not.toBe(-1);
+
+		inputWordArray.forEach(char => {
+			if (char === char.toUpperCase()) {
+				test('is upper case', () => {
+					expect(char === char.toUpperCase()).toBeTruthy();
+				});
+			} else {
+				test('is upper case', () => {
+					expect(char === char.toUpperCase()).toBeFalsy();
+				});
+			}
+
+			if (char.match(/[a-z]|[A-Z]/g)) {
+				test('matches pattern /[a-z]|[A-Z]/g', () => {
+					expect(char).toMatch(/[a-z]|[A-Z]/g);
+				});
+
+				test('find indexOf lowercase character inside the alphabet array', () => {
+					expect(indexOfFn(char, alphabet)).not.toBe(-1);
+				});
+			}
 		});
 	});
 
-	describe('when caesar13 is working properly', () => {});
-	describe('when caesar13 is not working properly', () => {});
-	describe('when returned caesar13 string is giving the input', () => {});
-	describe('when returned caesar13 string is not giving the input', () => {});
+	describe('when caesar13 is working properly', () => {
+		test('it should not change number position', () => {
+			const inputString = 'a1b';
+			expect(caesar13(inputString, rot)).toBe('n1o');
+		});
+		test('it should not change character case', () => {
+			const inputString = 'A1b';
+			expect(caesar13(inputString, rot)).toBe('N1o');
+		});
+		test('it should be equal', () => {
+			const inputString = 'a1b';
+			expect(caesar13(inputString, rot)).toBe('n1o');
+		});
+		test('it should be equal', () => {
+			const inputString = '0123456789';
+			expect(caesar13(inputString, rot)).toBe('0123456789');
+		});
+	});
+
+	describe('when reversing caesar13 result string should give the input', () => {
+		const input = 'a1b';
+		const result = caesar13(input, rot);
+		test('it should be equal', () => {
+			expect(result.toBe('1no'));
+		});
+		test('it should be equal', () => {
+			expect(caesar13(result, rot).toBe(input));
+		});
+	});
 });
