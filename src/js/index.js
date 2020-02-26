7c6d918183510567ec67a106c6b155eb200f2407
Robert Adamczewski <robert@adamczewski.me>
2020-02-26 21:07:54 +0100

    feat: create app functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,30 @@
-import caesar13 from './caesar13';
+import '../scss/style.scss';
+import caesar from './caesar';
+
+const rotSelect = document.querySelector('.rot-select--js');
+const inputText = document.querySelector('.input-text--js');
+const outputText = document.querySelector('.output-text--js');
+const rotOptions = [
+	...Array(26).keys()
+];
+
+rotOptions.forEach(option => {
+	const newOption = document.createElement('option');
+	newOption.value = option;
+	newOption.text = option;
+	if (option === 13) newOption.selected = true;
+	rotSelect.add(newOption);
+});
+
+function runCaesar(e) {
+	if (e.key === 'Enter') {
+		const rot = rotSelect.options[rotSelect.selectedIndex].value;
+
+		if (inputText.value !== '') {
+			console.log(caesar(inputText.value, rot));
+			outputText.textContent = caesar(inputText.value, rot);
+		}
+	}
+}
+
+inputText.addEventListener('keyup', runCaesar);
