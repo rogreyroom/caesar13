42a1224986e88ba3dc3160b55f36833ca2008331
Robert Adamczewski <robert@adamczewski.me>
2020-03-05 16:55:00 +0100

    refactor: change alphabet array to string
    michaljabi: alphabet może być zwykłym stringiem 'abcd....' dzieje się tak dlatego że, String.prototype również posiada indexOf + 2 sprawa, w JS String jest kolekcją iterowalną

--- src/js/caesar.js
+++ src/js/caesar.js
@@ -30,7 +30,7 @@ function caesar(input, rot) {
 function findNewChar(char, rot) {
 	let theRot = rot;
 	// prettier-ignore
-	const alphabet = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
+	const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 	const idx = alphabet.indexOf(char.toLowerCase());
 	let newIdx;
 	theRot = +theRot;
